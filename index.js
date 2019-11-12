2640a7a1d2d446fa9f9a07fc276b13009083b65c
Jason <jzzxhciu@gmail.com>
2019-11-12 11:38:26 +0800

    Working socketio Draw Version1, use wifi ip

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-var app = require('express')();
+var express = require('express');
+
+var app = express();
+
 var http = require('http').createServer(app);
+
+/*
+var server = app.listen(process.env.PORT || 3000,"127.0.0.1",listen);
+
+
+function listen(){
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('app listening at http://' + host + ':' + port);
+}*/
+
+http.listen(3000,'192.168.31.148',function(){
+  console.log('listening on *:3000');
+});
+
+app.use(express.static('public'));
+
 var io = require('socket.io')(http);
 
-var at = app();
 
-at.use(app.static('public'));
+
 
 app.get('/',function(req,res){
 
   res.sendFile(__dirname + '/index.html');
 });
+
 
 io.on('connection', function(socket){
     //console.log('a user connected');
@@ -27,20 +47,21 @@ io.on('connection', function(socket){
     socket.on('mouse' , function(msg3){
        
        io.emit('mouse', msg3);
-	  // console.log('x: ' + msg3.x + ", y: "+ msg3.y);
+	   //console.log('x: ' + msg3.x + ", y: "+ msg3.y);
    });
     socket.on('mouseDown' , function(msg4){
        
        io.emit('mouseDown', msg4);
 	  // console.log('x: ' + msg3.x + ", y: "+ msg3.y);
    });
+    socket.on('mousetouch' , function(msg5){
+       
+       io.emit('mousetouch', msg5);
+	  // console.log('x: ' + msg5.x + ", y: "+ msg5.y);
+   });   
       
    
    
 });
 
-http.listen(3000,function(){
 
-  console.log('listening on *:3000');
-
-});
---
+++ public/sketch.js
@@ -0,0 +1,96 @@
+
+var socket;
+var touch_state = 0;
+var capture;
+
+function setup(){
+    createCanvas(400, 400);
+    //background(0);
+    socket = io.connect('http://192.168.31.148:3000');
+    
+    capture =  createCapture({
+    audio: false,
+    video: {
+      facingMode: {
+        exact: "environment"
+      }
+    }
+  });    
+    capture.hide();
+    capture.size(320, 240);
+}
+
+function draw(){
+   
+   if(touches.length > 1){
+       background(255);
+   }
+   
+   image(capture, 0, 0, 320, 240);
+    
+}
+
+function touchStarted(){
+    
+    touch_state = 1;
+    //console.log("Touch_state: " + touch_state);
+    socket.emit("mouseDown", 1);
+    return false;
+}
+
+function touchEnded(){
+    
+    touch_state = 0;
+   // console.log("Touch_state: " + touch_state);
+    socket.emit("mouseDown", 0);
+    return false;
+}
+
+function touchMoved() {
+
+    fill(255,0,0);
+    noStroke();
+    ellipse(mouseX, mouseY, 15, 15);
+    sendmouse(int(mouseX),int(mouseY));
+  // prevent default
+  return false;
+}
+
+function mouseDragged() {
+  // Draw some white circles
+  fill(0,255,0);
+  noStroke();
+  ellipse(mouseX,mouseY,20,20);
+  // Send the mouse coordinates
+  sendmouse(mouseX,mouseY);
+}
+
+// Function for sending to the socket
+function sendmouse2(xpos, ypos) {
+  // We are sending!
+  console.log("sendTouch: " + xpos + " " + ypos);
+  
+  // Make a little object with  and y
+  var data = {
+    x: xpos,
+    y: ypos
+  };
+
+  // Send that object to the socket
+  socket.emit('mousetouch',data);
+}
+
+// Function for sending to the socket
+function sendmouse(xpos, ypos) {
+  // We are sending!
+  console.log("sendmouse: " + xpos + " " + ypos);
+  
+  // Make a little object with  and y
+  var data = {
+    x: xpos,
+    y: ypos
+  };
+
+  // Send that object to the socket
+  socket.emit('mouse',data);
+}
